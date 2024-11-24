110f451525e4188947cea16d61ebe6d3fc58b64b
StackMastery <labib.developers@gmail.com>
2024-11-24 11:12:25 -0500

    First Data Saved Via Express In Mongo Db Feeling Very Happy

--- index.js
+++ index.js
@@ -1,17 +1,47 @@
-import express from 'express'; // Import Dependencis
-import dotenv from 'dotenv'    // 
+import dotenv from 'dotenv'    // Import Dependencis
+import express from 'express'; // 
 import cors from 'cors'        // 
+import { MongoClient, ServerApiVersion } from 'mongodb';
 
 dotenv.config() // Dot Env Import 
 
 const app = express() // Express App
 const port = process.env.PORT || 3000 // PORT
+const mongoDBUri = process.env.MONGO_DB_URI // Mongo DB URi
+
+const client = new MongoClient(mongoDBUri, { // Mongo Db Client
+    serverApi: {
+      version: ServerApiVersion.v1,
+      strict: true,
+      deprecationErrors: true,
+    }
+  });
 
 app.use(cors())
+app.use(express.json())
 
-app.get('/', (req, res) => {
-    res.send('Say Hello')
-})
+;(async () => {
+    try{
+        await client.connect()
+        console.log(`Mongo Db Connection Succes`)
+
+        app.post('/user', async (req, res) => {
+            const body = req.body
+
+            const database = client.db("stackmastery")
+            const usersCollections = database.collection("users")
+
+            const result = await usersCollections.insertOne(body)
+
+            res.send(result.insertedId)
+
+        })
+    }
+    catch(err){
+        throw err
+        console.log(err)
+    }
+})();
 
 
 app.listen(port, () => {
